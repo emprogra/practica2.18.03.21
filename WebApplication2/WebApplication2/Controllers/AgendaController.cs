c9f94407740540b49e745c0ca3def02082856c8f
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-03-19 01:06:41 -0400

    programa completo

---
+++ WebApplication2/WebApplication2/Controllers/AgendaController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class AgendaController : ApiController
+    {
+        private DataContext db = new DataContext();
+
+        // GET: api/Agenda
+        public IQueryable<Agenda> GetAgenda()
+        {
+            return db.Agenda;
+        }
+
+        // GET: api/Agenda/5
+        [ResponseType(typeof(Agenda))]
+        public IHttpActionResult GetAgenda(string id)
+        {
+            Agenda agenda = db.Agenda.Find(id);
+            if (agenda == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(agenda);
+        }
+
+        // PUT: api/Agenda/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutAgenda(string id, Agenda agenda)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != agenda.Nombre)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(agenda).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AgendaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Agenda
+        [ResponseType(typeof(Agenda))]
+        public IHttpActionResult PostAgenda(Agenda agenda)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Agenda.Add(agenda);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (AgendaExists(agenda.Nombre))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = agenda.Nombre }, agenda);
+        }
+
+        // DELETE: api/Agenda/5
+        [ResponseType(typeof(Agenda))]
+        public IHttpActionResult DeleteAgenda(string id)
+        {
+            Agenda agenda = db.Agenda.Find(id);
+            if (agenda == null)
+            {
+                return NotFound();
+            }
+
+            db.Agenda.Remove(agenda);
+            db.SaveChanges();
+
+            return Ok(agenda);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AgendaExists(string id)
+        {
+            return db.Agenda.Count(e => e.Nombre == id) > 0;
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<WebApplication2.Models.Agenda> Agenda { get; set; }
+    }
+}
